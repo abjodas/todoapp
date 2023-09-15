40c00020c9ae4074bea8f0a80941237d154d6f93
abjodas <abjodas@gmail.com>
2023-09-15 11:42:05 +0200

    main

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, FlatList, Image, TextInput } from 'react-native';
+import { Text, View, StyleSheet, FlatList, Image, TextInput, Dimensions, TouchableOpacity } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import database from '@react-native-firebase/database';
 
@@ -7,6 +7,7 @@ import database from '@react-native-firebase/database';
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [text, onChangeText] = useState("")
 
   useEffect(() => {
     getDatabase();
@@ -26,15 +27,37 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-
+      <View style={[styles.container, { alignItems: 'center' }]}>
+        <TextInput style={styles.inputBox} placeholder={"Enter Text"} onChangeText={onChangeText} />
+        <TouchableOpacity style={styles.addButton} >
+          <Text style={{ color: "white" }}>Add</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const { height, width } = Dimensions.get("screen");
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  inputBox: {
+    width: width - 30,
+    marginTop: 20,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10
+  },
+  addButton: {
+    width: 100,
+    padding: 10,
+    backgroundColor: "#0D3B66",
+    marginTop: 10,
+    alignItems: "center",
+    borderRadius: 5
   }
 });
